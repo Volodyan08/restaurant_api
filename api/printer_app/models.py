f9a633a1666cd59086d260528b67269f796bd712
Volodymyr Vintila <vovavintila@gmail.com>
2023-01-08 19:27:53 +0100

    Add Printer and Check models

--- api/printer_app/models.py
+++ api/printer_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Printer(models.Model):
+    CHECK_TYPES = (
+
+    )
+    name = models.CharField(max_length=30)
+    api_key = models.CharField(max_length=200)
+    check_type = models.CharField()
+    point_id = models.IntegerField()
+
+
+class Check(models.Model):
+    printer = models.ForeignKey(Printer, on_delete=models.CASCADE)
+    type = models.CharField()
+    order = models.JSONField(default=0)
+    status = models.CharField(default=0)
+    pdf_file = models.FileField(default=0)
