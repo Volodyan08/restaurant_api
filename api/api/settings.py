1ac908c20499f95f15a3a62c3b18e339d3c92bfc
Volodymyr Vintila <vovavintila@gmail.com>
2023-01-15 19:52:36 +0100

    update models, add migration

--- api/api/settings.py
+++ api/api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'printer_app',
     'django_celery_results',
     'django_redis',
 ]
--- api/printer_app/models.py
+++ api/printer_app/models.py
@@ -1,22 +1,36 @@
+import uuid
 from django.db import models
-from datetime import datetime
-from choices import CHECK_TYPE, PRINTER_TYPE, CHECK_STATUS
 
 
-class CreateDateTimeModel(models.Model):
-    created = models.DateTimeField(verbose_name="Creation date and time", default=datetime.now)
-
-
-class Printer(CreateDateTimeModel, models.Model):
+class Printer(models.Model):
+    PRINTER_TYPE = (
+        ("K", "KITCHEN"),
+        ("C", "CLIENT"),
+    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     name = models.CharField(max_length=30)
     api_key = models.CharField(max_length=200)
-    check_type = models.CharField(choices=CHECK_TYPE)
+    check_type = models.CharField(choices=PRINTER_TYPE, max_length=32)
     point_id = models.IntegerField()
 
 
-class Check(CreateDateTimeModel, models.Model):
+class Check(models.Model):
+    CHECK_TYPE = (
+        ("K", "KITCHEN"),
+        ("C", "CLIENT"),
+    )
+    CHECK_STATUS = (
+        ("N", "NEW"),
+        ("R", "RENDERED"),
+        ("P", "PRINTED"),
+    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
     printer = models.ForeignKey(Printer, on_delete=models.CASCADE)
-    type = models.CharField(choices=PRINTER_TYPE)
+    type = models.CharField(choices=CHECK_TYPE, max_length=32)
     order = models.JSONField()
-    status = models.CharField(default=CHECK_STATUS.NEW, choices=CHECK_STATUS)
+    status = models.CharField(default="N", choices=CHECK_STATUS, max_length=32)
     pdf_file = models.FileField(default=0)
